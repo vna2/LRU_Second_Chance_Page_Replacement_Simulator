2a0154c6b8554b1a1734ae6ac501a9443ff04652
vna2 <vagelisnazaris@gmail.com>
2020-12-24 21:56:57 +0200

    lru working

--- main.cpp
+++ main.cpp
@@ -2,22 +2,23 @@
 #include "hash_table.hpp"
 #include "semaphores_sheared_mem.hpp"
 #include "list.hpp"
+//#include "hash_function.hpp"
 
 using namespace std;
 
 #define sh_mem_p1 sh_mem_p1
 #define sh_mem_p2 sh_mem_p2
 
-void P1(int algorithm,int q,int bucketsno);
+void P1(int algorithm,int q,int bucketsNo);
 void P2(int algorithm,int q,int bucketsno,hash_table *table);
-int hash_index(int data, int bucketsno);
+int returnHash(string s,int MAX_LEN);
 
 void lru(int q,int bucketsno);
 
 int main(int argc, char const *argv[]) {
-    int q=10000;
+    int q=1000000;
 
-    P1(1,q,5);
+    P1(1,q,100);
 
     // int pid = fork();
     // switch(pid){
@@ -32,9 +33,9 @@ int main(int argc, char const *argv[]) {
     return 0;
 }
 
-void P1(int algorithm,int q,int bucketsno){
+void P1(int algorithm,int q,int bucketsNo){
     if(algorithm==1)
-        lru(q,bucketsno);
+        lru(q,bucketsNo);
 }
 
 
@@ -43,25 +44,24 @@ void P2(int algorithm,int q,int bucketsno,hash_table *table){
 
 }
 
-void lru(int q,int bucketsno){
-    hash_table table(5,10);
-    listPg *saved_page=new listPg;
-    int address_num ;
+void lru(int q,int bucketsNo){
+    hash_table table(bucketsNo,10);
+    listPg *saved_pages=new listPg;
+    string address_num ;
     ifstream bzip_file("bzip.trace");
     int hash_num;
     stringstream hex_num;
     string line, addrStr, role;
     clock_t timer;
-    clock_t min;
 
     for (int i = 0; i < q; i++) {
         getline(bzip_file, line);
-        addrStr.assign(line.begin(),line.begin()+5);
+        address_num.assign(line.begin(),line.begin()+5);
         role.assign(line.end()-1,line.end());
         timer=clock();
 
-        hex_num<<addrStr;
-        hex_num>>hex>>address_num;
+        //hex_num<<addrStr;
+        // hash_index_num>>hex>>address_num;
         Page* page= new Page(address_num,role,timer);
 
 
@@ -69,10 +69,12 @@ void lru(int q,int bucketsno){
             table.write_counter++;
         else
             table.read_counter++;
-        hash_num=hash_index(address_num,bucketsno);
+        hash_num=returnHash(address_num,bucketsNo);
+        //hash_num=hash_index(address_num.c_str(),address_num.length(),bucketsno);
 
-        if(table.table[hash_num]->full){
-            if(table.table[hash_num]->find_replace(page)){
+
+        if(table.table[hash_num]->full==true){
+            if(table.table[hash_num]->find_replace(page)==0){
                 table.table[hash_num]->replace_lru(page);
                 table.page_faults++;
             }
@@ -86,11 +88,24 @@ void lru(int q,int bucketsno){
         }
 
     }
+    cout << "~~~~~~~~~~~Stats~~~~~~~~~~~~~~\n";
+    cout << "Page write: "<< table.write_counter<<endl;
+    cout << "Page read: "<< table.read_counter<<endl;
+    cout << "Page faults: "<< table.page_faults<<endl;
+
+    //table.print();
+
 
 }
 
-int hash_index(int data, int bucketsno){
-    int hash;
-    hash=data%bucketsno;
-    return hash;
+int returnHash(string s,int MAX_LEN)
+{
+    // A simple hashing, no collision handled
+    int sum=0,index=0;
+    for(string::size_type i=0; i < s.length(); i++)
+    {
+        sum += s[i];
+    }
+    index = sum % MAX_LEN;
+    return index;
 }
